55256f17e796ea2eefc88a9d4ea05d76544ff842
zhangran <15041256993@163.com>
2020-07-22 17:17:56 +0800

    部门表

---
+++ src/main/java/com/dj/personnel/pojo/Department.java
@@ -0,0 +1,34 @@
+package com.dj.personnel.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+/**
+ * 部门表
+ */
+@Data
+public class Department {
+    /**
+     * 部门id
+     */
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 部门名称
+     */
+    private String name;
+
+    /**
+     * 部门描述
+     */
+    private String remark;
+
+    /**
+     * 部门状态
+     * 0：删除
+     * 1：正常
+     */
+    private Integer isDel;
+}
