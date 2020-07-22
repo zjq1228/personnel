710f5f861d7723bff97a49a6c795221621128c8f
zjq18534166240 <zjq18534166240@163.com>
2020-07-22 17:15:17 +0800

    user表 仅结构

---
+++ src/main/java/com/dj/personnel/config/LoginConfig.java
@@ -0,0 +1,20 @@
+package com.dj.personnel.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class LoginConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //注册TestInterceptor拦截器
+        InterceptorRegistration registration = registry.addInterceptor(new com.dj.personnel.config.MyInterceptor());
+        //所有路径都被拦截
+        registration.addPathPatterns("/**");
+        //放过路径
+        registration.excludePathPatterns("/toLogin");
+    }
+}
