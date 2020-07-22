4de3cac016725067f81afae38bc98b474d32812f
zhangran <15041256993@163.com>
2020-07-22 15:44:53 +0800

    2

--- src/main/java/com/dj/personnel/PersonnelApplication.java
+++ src/main/java/com/dj/personnel/PersonnelApplication.java
@@ -1,9 +1,13 @@
 package com.dj.personnel;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@MapperScan("com.dj.personnel.mapper")
 public class PersonnelApplication {
 
     public static void main(String[] args) {
