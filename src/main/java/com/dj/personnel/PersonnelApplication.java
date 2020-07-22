058e2861456f6448bdf3e3b5b3bb23124dbc2886
zhangran <15041256993@163.com>
2020-07-22 15:45:48 +0800

    2

--- src/main/java/com/dj/personnel/PersonnelApplication.java
+++ src/main/java/com/dj/personnel/PersonnelApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * 启动类
+ */
 @SpringBootApplication
 @EnableTransactionManagement
 @MapperScan("com.dj.personnel.mapper")
