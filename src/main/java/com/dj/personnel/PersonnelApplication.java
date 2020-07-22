8acff13acd4cc5df799e4df0cf8258235e2c602d
zjq18534166240 <zjq18534166240@163.com>
2020-07-22 15:06:22 +0800

    第一次提交

---
+++ src/main/java/com/dj/personnel/PersonnelApplication.java
@@ -0,0 +1,13 @@
+package com.dj.personnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonnelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonnelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dj/personnel/PersonnelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dj.personnel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonnelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
